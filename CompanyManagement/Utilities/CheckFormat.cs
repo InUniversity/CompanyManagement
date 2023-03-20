6a378656e8fe9ab586feadd5828862d89c62cde8
AnhAnNek <info.vananne@gmail.com>
2023-03-20 15:01:30 +0700

    #61 add some constants to make more readable (again)

---
+++ CompanyManagement/Utilities/CheckFormat.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace CompanyManagement
+{
+    public static class CheckFormat
+    {
+        private const int IDENTIFY_CARD_LENGTH = 12;
+        private const int PASSWORD_MINIMUM_LENGTH = 8;
+
+        public static bool ValidateEmail(string email)
+        {
+            try
+            {
+                MailAddress checkmail = new MailAddress(email);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool ValidatePhoneNumber(string phoneNumber)
+        {
+            string pattern = @"^0[0-9]{9}$";
+            return Regex.IsMatch(phoneNumber, pattern);
+        }
+
+        public static bool ValidateIdentifyCard(string identifyCard)
+        {
+            return identifyCard.Length == IDENTIFY_CARD_LENGTH;
+        }
+
+        public static bool ValidatePassword(string password)
+        {
+            return password.Length >= PASSWORD_MINIMUM_LENGTH;
+        }
+    }
+}
