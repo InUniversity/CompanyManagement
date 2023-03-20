8e553722f4c40e0808b1dbf45af1c2099bf24df3
AnhAnNek <info.vananne@gmail.com>
2023-03-20 10:08:02 +0700

    #28 refactor datacontext, delete ManagerViewModel

--- CompanyManagement/Dialogs/UpdateProjectDialog.xaml.cs
+++ CompanyManagement/Dialogs/UpdateProjectDialog.xaml.cs
@@ -6,15 +6,12 @@ namespace CompanyManagement.Dialogs
     /// <summary>
     /// Interaction logic for UpdateProjectDialog.xaml
     /// </summary>
-    public partial class UpdateProjectDialog 
-        : Window
+    public partial class UpdateProjectDialog : Window
     {
         public UpdateProjectDialog()
         {
             InitializeComponent();
             DataContext = new UpdateProjectViewModel();
-            projectInputUC.DataContext = new ProjectInputViewModel();
-            ((UpdateProjectViewModel)DataContext).ProjectInputDataContext = (ProjectInputViewModel)projectInputUC.DataContext;
         }
     }
 }
