a05c48a16855901aefa5fbac17c1e1b4661015c1
AnhAnNek <info.vananne@gmail.com>
2023-03-20 15:00:20 +0700

    #62 add convert DateTimeToString, StringToDateTime

---
+++ CompanyManagement/Utilities/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace CompanyManagement
+{
+    public class Utils
+    {
+        public const string INVALIDATE_EMAIL_MESSAGE = "Email không hợp lệ!!!";
+        public const string INVALIDATE_PHONE_NUMBER_MESSAGE = "Số điện thoại không hợp lệ!!!";
+        public const string INVALIDATE_BIRTHDAY_MESSAGE = "Ngày sinh không hợp lệ!!!";
+        public const string INVALIDATE_IDENTIFY_CARD_MESSAGE = "CMND/CCCD không hợp lệ!!!";
+        public const string INVALIDATE_PASSWORK_MESSAGE = "Mật khẩu không hợp lệ!!!";
+        public const string INVALIDATE_EMPTY_MESSAGE = "Các thông tin không được để trống!!!";
+        public const string EXIST_ID_MESSAGE = "ID đã tồn tại!!!";
+        public const string EXIST_IDENTIFY_CARD_MESSAGE = "CMND/CCCD đã tồn tại!!!";
+        public const string EXIST_PHONE_NUMBER_MESSAGE = "Số điện thoại đã tồn tại!!!";
+
+        private const string FORMAT_DATEONLY = "dd-MM-yyyy";
+        private const string FORMAT_DATETIME = "g";
+        
+        public static string DateToString(DateTime dateOnly)
+        {
+            return dateOnly.ToString(FORMAT_DATEONLY);
+        }
+
+        public static DateTime StringToDate(string dateOnly)
+        {
+            return DateTime.ParseExact(dateOnly, FORMAT_DATEONLY, CultureInfo.InvariantCulture); 
+        }
+        
+        public static string DateTimeToString(DateTime dateTime)
+        {
+            return dateTime.ToString(FORMAT_DATETIME);
+        }
+
+        public static DateTime StringToDateTime(string dateOnly)
+        {
+            return DateTime.ParseExact(dateOnly, FORMAT_DATETIME, CultureInfo.InvariantCulture);
+        }
+    }
+}
