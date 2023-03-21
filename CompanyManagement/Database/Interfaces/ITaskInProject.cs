185f83202253a9488d8d1b318ed5eee61e191b97
AnhAnNek <info.vananne@gmail.com>
2023-03-21 22:50:54 +0700

    #3 fix parameter of abstract method

--- CompanyManagement/Database/Interfaces/ITaskInProject.cs
+++ CompanyManagement/Database/Interfaces/ITaskInProject.cs
@@ -6,7 +6,7 @@ namespace CompanyManagement.Database.Interfaces
     public interface ITaskInProject
     {
         void Add(TaskInProject task);
-        void Delete(TaskInProject task);
+        void Delete(string id);
         void Update(TaskInProject task);
         List<TaskInProject> SearchByProjectID(string projectID);
     }
