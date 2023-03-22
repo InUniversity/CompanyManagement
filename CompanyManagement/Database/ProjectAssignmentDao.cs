20bb3f1c504306216bc0128a7232a0ffa40e7ea3
AnhAnNek <info.vananne@gmail.com>
2023-03-22 08:01:36 +0700

    fix Dao

--- CompanyManagement/Database/ProjectAssignmentDao.cs
+++ CompanyManagement/Database/ProjectAssignmentDao.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CompanyManagement.Database
 {
-    public class ProjectAssignmentDao : IDao
+    public class ProjectAssignmentDao : BaseDao
     {
-        DBConnection dbConnection = new DBConnection();
         public List<Employee> GetEmployeesInProject(string projectID)
         {
             string sqlStr = $"SELECT * FROM {EMPLOYEE_TABLE} WHERE {EMPLOYEE_DEPARTMENT_ID} IN("+
