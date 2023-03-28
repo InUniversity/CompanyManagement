b27a4d846565af8043165525ea79d7fdc2deb447
ZaoQuynh <21110171@student.hcmute.edu.vn>
2023-03-28 22:39:55 +0700

    #28 create timekeeping

---
+++ CompanyManagement/Database/Interfaces/ITimeKeepingDao.cs
@@ -0,0 +1,12 @@
+﻿using CompanyManagement.Models;
+
+namespace CompanyManagement.Database.Interfaces
+{
+    public interface ITimeKeepingDao
+    {
+        void Add(TimeKeeping timeKeeping);
+        void Delete(TimeKeeping timeKeeping);
+        void Update(TimeKeeping timeKeeping);
+        TimeKeeping SearchByID(string timeKeepingID);
+    }
+}
