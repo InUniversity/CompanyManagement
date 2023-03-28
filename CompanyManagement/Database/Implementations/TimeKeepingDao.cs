22f52455037f8bf22523cae5e89c27aad2e71617
ZaoQuynh <21110171@student.hcmute.edu.vn>
2023-03-28 22:53:50 +0700

    #84 create timekeeping

---
+++ CompanyManagement/Database/Implementations/TimeKeepingDao.cs
@@ -0,0 +1,43 @@
+﻿using CompanyManagement.Database.Interfaces;
+using CompanyManagement.Models;
+using System.Collections.Generic;
+
+namespace CompanyManagement.Database.Implementations
+{
+    public class TimeKeepingDao : BaseDao, ITimeKeepingDao
+    {
+        public void Add(TimeKeeping timeKeeping)
+        {
+            string sqlStr = $"INSERT INTO {TIME_KEEPING_TABLE} ({TIME_KEEPING_TASK_ID}, {TIME_KEEPING_START_TIME}, " +
+                            $"{TIME_KEEPING_END_TIME}, {TIME_KEEPING_EMPLOYEE_ID}, {TIME_KEEPING_NOTES}, " +
+                            $"{TIME_KEEPING_CREATE_BY}) VALUE('{timeKeeping.TaskID}', '{timeKeeping.StartTime}', " +
+                            $"'{timeKeeping.EndTime}', '{timeKeeping.EmployeeID}', '{timeKeeping.Notes}', '{timeKeeping.CreateBy}')";
+
+            dbConnection.ExecuteNonQuery(sqlStr);
+        }
+
+        public void Delete(TimeKeeping timeKeeping)
+        {
+            string sqlStr = $"DELETE FROM {TIME_KEEPING_TABLE} WHERE {TIME_KEEPING_TASK_ID} = '{timeKeeping.TaskID}'";
+            dbConnection.ExecuteNonQuery(sqlStr);
+        }
+
+        public TimeKeeping SearchByID(string timeKeepingID)
+        {
+            string sqlStr = $"SELECT * FROM {TIME_KEEPING_TABLE} WHERE {TIME_KEEPING_TASK_ID} = '{timeKeepingID}'";
+            List<TimeKeeping> tasks = dbConnection.GetList(sqlStr, reader => new TimeKeeping(reader));
+            if (tasks.Count == 0)
+                return null;
+            return tasks[0];
+        }
+
+        public void Update(TimeKeeping timeKeeping)
+        {
+            string sqlStr = $"UPDATE {TIME_KEEPING_TABLE} SET {TIME_KEEPING_START_TIME} = '{timeKeeping.StartTime}', " +
+                            $"{TIME_KEEPING_END_TIME} = '{timeKeeping.EndTime}', {TIME_KEEPING_EMPLOYEE_ID} = '{timeKeeping.EmployeeID}', " +
+                            $"{TIME_KEEPING_NOTES} = '{timeKeeping.Notes}', {TIME_KEEPING_CREATE_BY} = '{timeKeeping.CreateBy}' " +
+                            $"WHERE {TIME_KEEPING_TASK_ID} = '{timeKeeping.TaskID}';";
+            dbConnection.ExecuteNonQuery(sqlStr);
+        }
+    }
+}
---
+++ CompanyManagement/Models/TimeKeeping.cs
@@ -0,0 +1,82 @@
+﻿using CompanyManagement.Database;
+using System;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace CompanyManagement.Models
+{
+    public class TimeKeeping
+    {
+        private string taskID;
+        private string startTime;
+        private string endTime;
+        private string employeeeID;
+        private string notes;
+        private string createBy;
+
+        public string TaskID
+        {
+            get => taskID;
+            set => taskID = value;
+        }
+
+        public string StartTime
+        {
+            get => startTime;
+            set => startTime = value;
+        }
+
+        public string EndTime
+        {
+            get => endTime;
+            set => endTime = value;
+        }
+
+        public string EmployeeID
+        {
+            get => employeeeID;
+            set => employeeeID = value;
+        }
+
+        public string Notes
+        {
+            get => notes;
+            set => notes = value;
+        }
+
+        public string CreateBy
+        {
+            get => createBy;
+            set => createBy = value;
+        }
+
+        public TimeKeeping() { }
+
+        public TimeKeeping(string taskID, string startTime, string endTime, string employeeeID, string notes, string createBy)
+        {
+            this.taskID = taskID;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.employeeeID = employeeeID;
+            this.notes = notes;
+            this.createBy = createBy;
+        }
+
+        public TimeKeeping(SqlDataReader reader)
+        {
+            try
+            {
+                taskID = (string)reader[BaseDao.TIME_KEEPING_TASK_ID];
+                startTime = (string)reader[BaseDao.TIME_KEEPING_START_TIME];
+                endTime = (string)reader[BaseDao.TIME_KEEPING_END_TIME];
+                employeeeID = (string)reader[BaseDao.TIME_KEEPING_EMPLOYEE_ID];
+                notes = (string)reader[BaseDao.TIME_KEEPING_NOTES];
+                createBy = (string)reader[BaseDao.TIME_KEEPING_CREATE_BY];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
