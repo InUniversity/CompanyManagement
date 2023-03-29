c2298f5f91d69619d756c2dda1f62b5a5a595399
AnhAnNek <info.vananne@gmail.com>
2023-03-29 10:57:13 +0700

    #27 show departments can assign, departments in project (unfinished)

--- CompanyManagement/ViewModels/UserControls/ProjectInputViewModel.cs
+++ CompanyManagement/ViewModels/UserControls/ProjectInputViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using CompanyManagement.Database.Implementations;
 using CompanyManagement.Database.Interfaces;
 using CompanyManagement.Models;
 using CompanyManagement.Utilities;
@@ -14,10 +17,10 @@ namespace CompanyManagement.ViewModels.UserControls
         void TrimAllTexts();
         void RetrieveProject(Project project);
     }
-    
+
     public class ProjectInputViewModel : BaseViewModel, IProjectInput
     {
-        
+
         private string id = "";
         public string ID { get => id; set { id = value; OnPropertyChanged(); } }
 
@@ -39,8 +42,16 @@ namespace CompanyManagement.ViewModels.UserControls
         private ObservableCollection<Department> departmentsInProject;
         public ObservableCollection<Department> DepartmentsInProject { get => departmentsInProject; set { departmentsInProject = value; OnPropertyChanged(); } }
 
-        private ObservableCollection<Department> departmentsCanAssign;
-        public ObservableCollection<Department> DepartmentsCanAssign { get => departmentsCanAssign; set { departmentsCanAssign = value; OnPropertyChanged(); } }
+        private bool isOpenAddDeparmentDialog = false;
+        public bool IsOpenAddDeparmentDialog { get => isOpenAddDeparmentDialog; set { isOpenAddDeparmentDialog = value; OnPropertyChanged(); } }
+
+        private List<Department> departmentsCanAssign;
+        public List<Department> DepartmentsCanAssign { get => departmentsCanAssign; set { departmentsCanAssign = value; OnPropertyChanged(); } }
+
+        public ICommand OpenAddDepartmentsDialog { get; set; }
+        public ICommand CloseAddDepartmentDialog { get; set; }
+        public ICommand AddDepartmentCommand { get; set; }
+        public ICommand DeleteDeparmentCommand { get; set; }
 
         private IProjectAssignmentDao projectAssignmentDao;
 
@@ -49,23 +60,58 @@ namespace CompanyManagement.ViewModels.UserControls
             this.projectAssignmentDao = projectAssignmentDao;
             LoadDepartmentsInProject();
             LoadDepartmentsCanAssign();
+            SetCommands();
         }
 
         private void LoadDepartmentsInProject()
         {
             var departments = projectAssignmentDao.GetAllDepartmentInProject(ID);
-            DepartmentsInProject = new ObservableCollection<Department>(departments);
+            // DepartmentsInProject = new ObservableCollection<Department>(new DepartmentDao().GetAll());
+            var depart = new List<Department>()
+            {
+                new Department("hihi", "IT 1", "EM001"),
+                new Department("hihi", "IT 2", "EM001"),
+                new Department("hihi", "IT 3", "EM001"),
+                new Department("hihi", "IT 4", "EM001"),
+                new Department("hihi", "IT 5", "EM001"),
+                new Department("hihi", "IT 6", "EM001")
+            };
+            DepartmentsInProject = new ObservableCollection<Department>(depart);
         }
 
         private void LoadDepartmentsCanAssign()
         {
             var departments = projectAssignmentDao.GetDepartmentsCanAssignWork("03/12/2000 12:00 AM", "03/12/2022 12:00 AM");
-            DepartmentsInProject = new ObservableCollection<Department>(departments);
+            //DepartmentsInProject = new ObservableCollection<Department>(departments);
+            DepartmentsCanAssign = new DepartmentDao().GetAll();
+        }
+
+        private void SetCommands()
+        {
+            OpenAddDepartmentsDialog = new RelayCommand<object>(p => IsOpenAddDeparmentDialog = true);
+            CloseAddDepartmentDialog = new RelayCommand<object>(p => IsOpenAddDeparmentDialog = false);
+            AddDepartmentCommand = new RelayCommand<Department>(ExecuteAddDepartmentCommand);
+            DeleteDeparmentCommand = new RelayCommand<string>(ExecuteDeleteDepartmentCommand);
+        }
+
+        private void ExecuteAddDepartmentCommand(Department department)
+        {
+            projectAssignmentDao.Add(new ProjectAssignment(ID, department.ID));
+            LoadDepartmentsInProject();
+            LoadDepartmentsCanAssign();
+        }
+
+        private void ExecuteDeleteDepartmentCommand(string departmentID)
+        {
+            MessageBox.Show("hihi");
+            //projectAssignmentDao.Delete(new ProjectAssignment(ID, departmentID));
+            //LoadDepartmentsInProject();
+            //LoadDepartmentsCanAssign();
         }
 
         public Project CreateProjectInstance()
         {
-            return new Project(ID, Name, Utils.DateToString(Start), Utils.DateToString(End), Progress);
+            return new Project(ID, Name, Utils.DateTimeToString(Start), Utils.DateTimeToString(End), Progress);
         }
 
         public bool CheckAllFields()
