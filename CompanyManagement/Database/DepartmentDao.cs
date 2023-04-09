2feb06a16ae48237f7b87bf192a09a4be090da1f
ZaoQuynh <21110171@student.hcmute.edu.vn>
2023-04-09 11:19:49 +0700

    #127 CRUD for department completed

--- CompanyManagement/Database/DepartmentDao.cs
+++ CompanyManagement/Database/DepartmentDao.cs
@@ -6,6 +6,27 @@ namespace CompanyManagement.Database
 {
     public class DepartmentDao : BaseDao
     {
+
+        public void Add(Department department)
+        {
+            string sqlStr = $"INSERT INTO {DEPARTMENT_TABLE} ({DEPARTMENT_ID}, {DEPARTMENT_NAME}, {DEPARTMENT_MANAGER_ID})" +
+                            $"VALUES ('{department.ID}', '{department.Name}', '{department.ManagerID}')";
+            dbConnection.ExecuteNonQuery(sqlStr);
+        }
+
+        public void Delete(string id)
+        {
+            string sqlStr = $"DELETE FROM {DEPARTMENT_TABLE} WHERE {DEPARTMENT_ID} = '{id}'";
+            dbConnection.ExecuteNonQuery(sqlStr);
+        }
+
+        public void Update(Department department)
+        {
+            string sqlStr = $"UPDATE {DEPARTMENT_TABLE} SET {DEPARTMENT_NAME} = '{department.Name}', " +
+                            $"{DEPARTMENT_MANAGER_ID} = '{department.ManagerID}' WHERE {DEPARTMENT_ID} = '{department.ID}'";
+            dbConnection.ExecuteNonQuery(sqlStr);
+        }
+
         public List<Department> GetAll()
         {
             string sqlStr = $"SELECT * FROM {DEPARTMENT_TABLE}";
---
+++ CompanyManagement/Views/UserControls/DepartmentInputUC.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CompanyManagement.ViewModels.UserControls;
+using System.Windows.Controls;
+
+namespace CompanyManagement.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DepartmentInputUC.xaml
+    /// </summary>
+    public partial class DepartmentInputUC : UserControl
+    {
+        public DepartmentInputUC()
+        {
+            InitializeComponent();
+            DataContext = new DepartmentInputViewModel();
+        }
+    }
+}
