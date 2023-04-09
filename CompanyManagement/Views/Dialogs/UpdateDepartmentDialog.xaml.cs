3ccadf6c9c9b26674f9ebd7fa911528f182013bf
ZaoQuynh <21110171@student.hcmute.edu.vn>
2023-04-09 16:56:33 +0700

    #127 CRUD Departmennt .net famework completed

---
+++ CompanyManagement/Views/Dialogs/UpdateDepartmentDialog.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CompanyManagement.ViewModels.Dialogs;
+using System.Windows;
+
+namespace CompanyManagement.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for UpdateDepartmentDialog.xaml
+    /// </summary>
+    public partial class UpdateDepartmentDialog : Window
+    {
+        public UpdateDepartmentDialog()
+        {
+            InitializeComponent(); 
+            DataContext = new UpdateDepartmentViewModel();
+        }
+    }
+}
