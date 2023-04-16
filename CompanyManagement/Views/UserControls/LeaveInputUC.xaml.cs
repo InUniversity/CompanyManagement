c3e28cf0c083a50783a2900b85f41407d7b14cfe
TruongAnn2003 <nguyentruongan281203@gmail.com>
2023-04-17 00:12:22 +0700

    #137 fix view leave

--- CompanyManagement/Views/UserControls/LeaveInputUC.xaml.cs
+++ CompanyManagement/Views/UserControls/LeaveInputUC.xaml.cs
@@ -1,4 +1,18 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CompanyManagement.ViewModels;
 using CompanyManagement.ViewModels.UserControls;
 
 namespace CompanyManagement.Views.UserControls
@@ -10,6 +24,7 @@ namespace CompanyManagement.Views.UserControls
     {
         public LeaveInputUC()
         {
+            InitializeComponent();
             DataContext = new LeaveInputViewModel();
         }
     }
