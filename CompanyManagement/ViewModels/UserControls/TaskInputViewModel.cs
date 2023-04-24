9042e1b6d80736e33917582ae7d80124243ba50a
AnhAnNek <info.vananne@gmail.com>
2023-04-24 17:32:37 +0700

    #28 TaskInputVM use property to replace get/set method

--- CompanyManagement/ViewModels/UserControls/TaskInputViewModel.cs
+++ CompanyManagement/ViewModels/UserControls/TaskInputViewModel.cs
@@ -7,50 +7,40 @@ using System.Windows.Input;
 using System.Linq;
 using System.Windows.Controls;
 using CompanyManagement.Utilities;
-using CompanyManagement.Views.UserControls;
 
 namespace CompanyManagement.ViewModels.UserControls
 {
     public interface ITaskInput
     {
-        TaskInProject CreateTaskInProjectInstance();
+        TaskInProject Task { get; set; }
         bool CheckAllFields();
         void TrimAllTexts();
-        void RetrieveTask(TaskInProject taskInProject);
     }
 
-    public class TaskInputViewModel : BaseViewModel, ITaskInput 
+    public class TaskInputViewModel : BaseViewModel, ITaskInput
     {
+        private TaskInProject task;
+        public TaskInProject Task
+        {
+            get => task;
+            set
+            {
+                task = value;
+                Employees = assignmentDao.GetEmployeesInProject(task.ProjectID);
+                SearchedEmployeesCanAssign = new List<Employee>(Employees); 
+            }
+        }
         
-        private string id = "";
-        public string ID { get => id; set { id = value; OnPropertyChanged(); } }
-
-        private string title = "";
-        public string Title { get => title; set { title = value; OnPropertyChanged(); } }
-
-        private string description = "";
-        public string Description { get => description; set { description = value; OnPropertyChanged(); } }
-
-        private DateTime assignDate = DateTime.Now;
-        public DateTime AssignDate { get => assignDate; set { assignDate = value; OnPropertyChanged(); } }
-
-        private DateTime deadline = DateTime.Now;
-        public DateTime Deadline { get => deadline; set { deadline = value; OnPropertyChanged(); } }
-
-        private string createBy = CurrentUser.Ins.EmployeeIns.ID;
-        public string CreateBy { get => createBy; set { createBy = value; OnPropertyChanged(); } }
-
-        private string progress = "";
-        public string Progress { get => progress; set { progress = value; OnPropertyChanged(); } }
-
-        private string employeeID = "";
-        public string EmployeeID { get => employeeID; set { employeeID = value; OnPropertyChanged(); } }
-
-        private string projectID = "";
-        public string ProjectID { get => projectID; set { projectID = value; OnPropertyChanged(); } }
-
-        private string statusID = "";
-        public string StatusID { get => statusID; set { statusID = value; OnPropertyChanged(); } }
+        public string ID { get => task.ID; set { task.ID = value; OnPropertyChanged(); } }
+        public string Title { get => task.Title; set { task.Title = value; OnPropertyChanged(); } }
+        public string Description { get => task.Description; set { task.Description = value; OnPropertyChanged(); } }
+        public DateTime AssignDate { get => task.AssignDate; set { task.AssignDate = value; OnPropertyChanged(); } }
+        public DateTime Deadline { get => task.Deadline; set { task.Deadline = value; OnPropertyChanged(); } }
+        public string CreateBy { get => task.CreateBy; set { task.CreateBy = value; OnPropertyChanged(); } }
+        public string Progress { get => task.Progress; set { task.Progress = value; OnPropertyChanged(); } }
+        public string EmployeeID { get => task.EmployeeID; set { task.EmployeeID = value; OnPropertyChanged(); } }
+        public string ProjectID { get => task.ProjectID; set { task.ProjectID = value; OnPropertyChanged(); } }
+        public string StatusID { get => task.StatusID; set { task.StatusID = value; OnPropertyChanged(); } }
 
         private string errorMessage = "";
         public string ErrorMessage { get => errorMessage; set { errorMessage = value; OnPropertyChanged(); } }
@@ -59,7 +49,6 @@ namespace CompanyManagement.ViewModels.UserControls
         public List<Employee> Employees { get => employees; set { employees = value; OnPropertyChanged(); } }
 
         private List<TaskStatus> taskStatuses;
-
         public List<TaskStatus> TaskStatuses { get => taskStatuses; set { taskStatuses = value; OnPropertyChanged(); } }
 
         private List<Employee> searchedEmployeesCanAssign;
@@ -69,26 +58,22 @@ namespace CompanyManagement.ViewModels.UserControls
         public string TextToSearch { get => textToSearch; set { textToSearch = value; OnPropertyChanged(); SearchByName(); } }
 
         public ICommand AddEmployeeCommand { get; set; }
-        public ICommand GetSelectedEmployeeCommand { get; set; }
+        public ICommand GetSelectedEmployeeCommand { get; }
 
-        private TaskStatusDao taskStatusDao;
-        private ProjectAssignmentDao assignmentDao;
+        private TaskStatusDao taskStatusDao = new TaskStatusDao();
+        private ProjectAssignmentDao assignmentDao = new ProjectAssignmentDao();
 
         public TaskInputViewModel()
         {
-            taskStatusDao = new TaskStatusDao();
-            assignmentDao = new ProjectAssignmentDao();            
             GetSelectedEmployeeCommand = new RelayCommand<ListView>(ExecuteGetSelectedEmployeeCommand);       
             SetAllComboBox();
         }
 
         private void ExecuteGetSelectedEmployeeCommand(ListView listView)
         {
-            if(listView.SelectedItem != null)
-            {
-                var selectedItem = listView.SelectedItem as Employee;
-                EmployeeID = selectedItem.ID;
-            }       
+            if (listView.SelectedItem == null) return;
+            var selectedItem = listView.SelectedItem as Employee;
+            EmployeeID = selectedItem.ID;
         }
 
         private void SetAllComboBox()
@@ -96,26 +81,22 @@ namespace CompanyManagement.ViewModels.UserControls
             TaskStatuses = taskStatusDao.GetAll();
         }
 
-        public TaskInProject CreateTaskInProjectInstance()
-        {
-            return new TaskInProject(id, title, description, assignDate, deadline, progress, createBy, employeeID, projectID, statusID);
-        }
-
         public bool CheckAllFields()
         {
             ErrorMessage = "";
             if (string.IsNullOrWhiteSpace(Title) || string.IsNullOrWhiteSpace(EmployeeID))
             {
-                ErrorMessage = "Các thông tin không được để trống!!!";
+                ErrorMessage = Utils.INVALIDATE_EMPTY_MESSAGE;
                 return false;
             }
-            if (Deadline < AssignDate)
+            if (!CheckFormat.ValidateTimeline(AssignDate, Deadline))
             {
-                ErrorMessage = "Thời gian kết thúc không hợp lệ!!!";
+                ErrorMessage = Utils.INVALIDATE_TIMELINE;
                 return false;
             }
             return true;
         }
+        
         private void SearchByName()
         {
             var searchedItems = employees;
@@ -130,29 +111,10 @@ namespace CompanyManagement.ViewModels.UserControls
 
         public void TrimAllTexts()
         {
-            ID = id.Trim();
-            Title = title.Trim();
-            Description = description.Trim();
-            CreateBy = createBy.Trim();
-            Progress = progress.Trim(); 
-            EmployeeID = employeeID.Trim();
-            ProjectID = projectID.Trim();
-        }
-
-        public void RetrieveTask(TaskInProject taskInProject)
-        {
-            ID = taskInProject.ID;
-            Title = taskInProject.Title;
-            Description = taskInProject.Description;
-            AssignDate = taskInProject.AssignDate;
-            Deadline = taskInProject.Deadline;
-            CreateBy = taskInProject.CreateBy;
-            Progress = taskInProject.Progress;
-            EmployeeID = taskInProject.EmployeeID;
-            ProjectID = taskInProject.ProjectID;
-            StatusID = taskInProject.Status;
-            Employees = assignmentDao.GetEmployeesInProject(taskInProject.ProjectID);
-            SearchedEmployeesCanAssign = new List<Employee>(Employees);
+            Title = Title.Trim();
+            Description = Description.Trim();
+            CreateBy = CreateBy.Trim();
+            Progress = Progress.Trim(); 
         }
     }
 }
