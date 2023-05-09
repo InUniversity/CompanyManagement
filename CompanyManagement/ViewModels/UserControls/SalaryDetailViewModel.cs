e6699e5fb968005dcdcd9c19126c98e1a0dfc9ef
AnhAnNek <info.vananne@gmail.com>
2023-05-10 01:20:47 +0700

    delete setter of TotalOffDays in SalaryDetailViewModel

--- CompanyManagement/ViewModels/UserControls/SalaryDetailViewModel.cs
+++ CompanyManagement/ViewModels/UserControls/SalaryDetailViewModel.cs
@@ -15,7 +15,7 @@ namespace CompanyManagement.ViewModels.UserControls
         public string EmployeeID { get => salaryRecordIns.EmployeeID; set { salaryRecordIns.EmployeeID = value; OnPropertyChanged(); } }
         public DateTime MonthYear { get => salaryRecordIns.MonthYear; set { salaryRecordIns.MonthYear = value; OnPropertyChanged(); } }
         public int TotalWorkDays { get => salaryRecordIns.TotalWorkDays; set { salaryRecordIns.TotalWorkDays = value; OnPropertyChanged(); } }
-        public int TotalOffDays { get => salaryRecordIns.TotalOffDays; set { salaryRecordIns.TotalOffDays = value; OnPropertyChanged(); } }
+        public int TotalOffDays { get => salaryRecordIns.TotalOffDays; }
         public decimal TotalBonuses { get => salaryRecordIns.TotalBonuses; set { salaryRecordIns.TotalBonuses = value; OnPropertyChanged(); } }
         public decimal Income { get => salaryRecordIns.Income; set { salaryRecordIns.Income = value; OnPropertyChanged(); } }
         public Employee Worker { get => salaryRecordIns.Worker; }
