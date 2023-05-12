93f3592f1c9ebfb6773492aa3d087247d1a553dd
AnhAnNek <info.vananne@gmail.com>
2023-05-12 12:49:44 +0700

    #219 deleted all status tables

--- CompanyManagement/Enums/EPermission.cs
+++ CompanyManagement/Enums/EPermission.cs
@@ -5,14 +5,14 @@ namespace CompanyManagement.Enums
     public enum EPermission
     {
         [Description("Manager")]
-        Mgr,
+        Mgr = 1,
         [Description("Department Head")]
-        DepHead, 
+        DepHead = 2, 
         [Description("Normal Employee")] 
-        NorEmpl,  
+        NorEmpl = 3,  
         [Description("Human Resource")] 
-        HR, 
+        HR = 4, 
         [Description("Not allow")] 
-        NotAllow
+        NotAllow = 5
     }
 }
